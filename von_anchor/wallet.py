7fd24d5b1cb3e9c1c31d2c79bb629b962809278a
Ian Costanzo <ian@anon-solutions.ca>
2018-09-09 16:20:53 -0700

    Added logging to wallet create

--- von_anchor/wallet.py
+++ von_anchor/wallet.py
@@ -226,9 +226,9 @@ class Wallet:
         """
 
         LOGGER.debug('Wallet.create >>>')
-        println('Wallet.create >>>')
-        println("Config:", json.dumps(self.cfg))
-        println("Creds", json.dumps(self.access_creds))
+        print('Wallet.create >>>')
+        print("Config:", json.dumps(self.cfg))
+        print("Creds", json.dumps(self.access_creds))
 
         try:
             await wallet.create_wallet(
